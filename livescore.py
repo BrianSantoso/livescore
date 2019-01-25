b0647ea5d0b1bf8d4621f936cbedcb8e9bc00e8c
Brian Santoso <btsantoso@gmail.com>
2019-01-25 03:45:42 -0800

    cleaned up code

--- livescore.py
+++ livescore.py
@@ -2,6 +2,9 @@ import urllib3
 import time as TIME
 from bs4 import BeautifulSoup, SoupStrainer
 from datetime import datetime
+
+
+
 # https://www.livescore18.com/
 # https://www.livescore18.com/data/bf_en2.js?1548379839000
 http = urllib3.PoolManager()
@@ -26,55 +29,48 @@ data = []
 file = open('data.txt','w')
 file.truncate(0)
 
-
 for line in lines:
+
+	finished = line[findnth(line, ',', 11):findnth(line, ',', 12)][1:]
+	if finished != '-1':
+		continue
+
 	team1 = line[findnth(line, ',', 3):findnth(line, ',', 4)][2:-1]
 	team2 = line[findnth(line, ',', 4):findnth(line, ',', 5)][2:-1]
 
-	date = line[findnth(line, ',', 5):findnth(line, ',', 8)][2:].replace(',', '/')
-	year = 0
-	month = ''
-	try:
-		year = int(date[0:findnth(date, '/', 0)])
-		month = date[findnth(date, '/', 0) + 1:findnth(date, '/', 1)]
-		month = int(month) + 1
-		month = str(month)
-		if len(month) < 2:
-			month = '0' + month
-	except:
-		print(line)
-		continue
+	date = line[findnth(line, ',', 5):findnth(line, ',', 8)][2:]
+	year = month = 0
 
+	year = int(date[0:findnth(date, ',', 0)])
 
-	
-	
-	day = date[findnth(date, '/', 1) + 1:]
-	if len(day)<2:
-		day = int('0' + day)
+	month = date[findnth(date, ',', 0) + 1:findnth(date, ',', 1)]
+	month = int(month) + 1
+	month = str(month)
+	if len(month) < 2:
+		month = '0' + month
+
+	day = date[findnth(date, ',', 1) + 1:]
+	if len(day) < 2:
+		day = '0' + day
 	day = int(day)
 
 
 	time = line[findnth(line, ',', 8):findnth(line, ',', 10)][1:]
-	hour = time[0:findnth(time, ',', 0)]
 	minute = time[findnth(time, ',', 0) + 1:]
-	# timezone = line[findnth(line, ',', 17):findnth(line, ',', 18)][1:]
-	# timezone = int(timezone)
-	# timezone_difference = 2 - (2 * timezone)
-	
 
 	timezone_difference = int(-TIME.timezone / 3600)
-	hour = str((int(hour) + timezone_difference) % 24)
+	hour = time[0:findnth(time, ',', 0)]
+	hour = int(hour) + timezone_difference
+	hour = hour % 24
+	hour = str(hour)
 
 	if len(hour) < 2:
 		hour = '0' + hour
-
-
-	finished = line[findnth(line, ',', 11):findnth(line, ',', 12)][1:]
-	# print(finished)
+	
 	half = line[findnth(line, ',', 12):findnth(line, ',', 14)][1:].replace(',', '-')
 	full = line[findnth(line, ',', 14):findnth(line, ',', 16)][1:].replace(',', '-')
 
-	if finished == '-1':
-		file.write(str(day) + '/' + str(month) + '/' + str(year) + '|' + hour + ':' + minute + '|' + team1 + ':' + team2 + '|' + half + '|' + full + '\n')
+	data = str(day) + '/' + str(month) + '/' + str(year) + '|' + str(hour) + ':' + str(minute) + '|' + team1 + ':' + team2 + '|' + half + '|' + full + '\n'
+	file.write(data)
 
 file.close()
